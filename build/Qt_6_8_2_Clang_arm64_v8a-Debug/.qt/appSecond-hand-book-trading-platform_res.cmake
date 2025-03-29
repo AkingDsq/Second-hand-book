5f5c1015f36f4a806f47d30fd7d78d9cb7d86670
AkingDsq <3241208202@qq.com>
2025-03-29 16:36:13 +0800

    创建Qt quick项目

---
+++ build/Qt_6_8_2_Clang_arm64_v8a-Debug/.qt/appSecond-hand-book-trading-platform_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appSecond-hand-book-trading-platform")
+set(working_dir "D:/git/work/Second-hand-book-trading-platform")
+set(src_and_dest_list
+
+)
+set(timestamp_file "D:/git/work/Second-hand-book-trading-platform/build/Qt_6_8_2_Clang_arm64_v8a-Debug/.qt/appSecond-hand-book-trading-platform_res.txt")
